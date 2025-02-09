2cc15ca103bb0dd15b1cb4027a86964d7adf9bae
MANA-github <153607046+MANA-github@users.noreply.github.com>
2025-02-10 01:41:36 +0900

    test

---
+++ index.js
@@ -0,0 +1,26 @@
+function sendUserData() {
+    Fingerprint2.get((components) => {
+    let values = components.map(component => component.value);
+    let fingerprint = Fingerprint2.x64hash128(values.join(''), 31);
+
+    fetch('https://empty-moon-5b1b.manawork79.workers.dev', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+        fingerprint: fingerprint,
+        timestamp: new Date().toISOString(),
+        action: 'page_visit'
+        })
+    }).then(response => console.log('Sent to Cloudflare:', response))
+        .catch(error => console.error('Error:', error));
+    });
+}
+sendUserData();
+
+if (!localStorage.getItem('user_id')) {
+    const userId = uuidv4();
+    localStorage.setItem('user_id', userId);
+} else {
+    const userId = localStorage.getItem('user_id');
+}
+console.log(userId);
