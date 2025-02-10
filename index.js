93a5740a5b2754192af4a9b2f18f4b79cb29c27e
MANA-github <153607046+MANA-github@users.noreply.github.com>
2025-02-10 09:42:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 import { v4 as uuidv4 } from 'uuid';
 
-function sendUserData() {
-    Fingerprint2.get((components) => {
-    let values = components.map(component => component.value);
-    let fingerprint = Fingerprint2.x64hash128(values.join(''), 31);
+async function sendUserData() {
+    if (window.Fingerprint2) {
+        const components = await new Promise(resolve => {
+            Fingerprint2.get(resolve);
+        });
 
-    fetch('https://empty-moon-5b1b.manawork79.workers.dev', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-        fingerprint: fingerprint,
-        timestamp: new Date().toISOString(),
-        action: 'page_visit'
-        })
-    }).then(response => console.log('Sent to Cloudflare:', response))
-        .catch(error => console.error('Error:', error));
-    });
+        let values = components.map(component => component.value);
+        let fingerprint = Fingerprint2.x64hash128(values.join(''), 31);
+
+        fetch('https://empty-moon-5b1b.manawork79.workers.dev', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                fingerprint: fingerprint,
+                timestamp: new Date().toISOString(),
+                action: 'page_visit'
+            })
+        }).then(response => console.log('Sent to Cloudflare:', response))
+          .catch(error => console.error('Error:', error));
+    } else {
+        console.error("Fingerprint2 is not available.");
+    }
 }
 sendUserData();
 
+let userId;
 if (!localStorage.getItem('user_id')) {
-    const userId = uuidv4();
+    userId = uuidv4();
     localStorage.setItem('user_id', userId);
 } else {
-    const userId = localStorage.getItem('user_id');
+    userId = localStorage.getItem('user_id');
 }
 console.log(userId);
